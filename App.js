b9bde481d65d007efd7076d1827725c36a80adbb
buketsoyhan <45885283+buketsoyhan@users.noreply.github.com>
2021-01-27 23:09:00 +0300

    Bug fixed

--- App.js
+++ App.js
@@ -1,34 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import * as firebase from 'firebase';
+import React,{Component} from 'react';
+import * as firebase from "firebase";
+import * as FirebaseCore from 'expo-firebase-core'
 import { StyleSheet, Text, View } from 'react-native';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCUglAox_6DJ83TLMmhQvTBvhIJgNyso98",
-  authDomain: "news-site-30650.firebaseapp.com",
-  projectId: "news-site-30650",
-  storageBucket: "news-site-30650.appspot.com",
-  messagingSenderId: "647658439837",
-  appId: "1:647658439837:web:8e96807dc6d83c18c0c074",
-  measurementId: "G-6ZS3XKZL11"
-};
-// Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+export default class App extends Component {
+  
 
-export default function App() {
+componentDidMount = () => {
+  console.log(FirebaseCore.DEFAULT_APP_OPTIONS);
+}
+
+render(){
   return (
-    <View style={styles.container}>
-      <Text>Buket Ajda Tarik</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Ajda buket tarık</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
