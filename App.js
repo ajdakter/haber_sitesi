ceb1c2c0e9c72b4df2f0f31d3f8c118aef31bb1d
buketsoyhan <buketsoyhan1998@outlook.com>
2021-01-27 20:46:01 +0300

    Firebase authentication installed.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as firebase from 'firebase';
 import { StyleSheet, Text, View } from 'react-native';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCUglAox_6DJ83TLMmhQvTBvhIJgNyso98",
+  authDomain: "news-site-30650.firebaseapp.com",
+  projectId: "news-site-30650",
+  storageBucket: "news-site-30650.appspot.com",
+  messagingSenderId: "647658439837",
+  appId: "1:647658439837:web:8e96807dc6d83c18c0c074",
+  measurementId: "G-6ZS3XKZL11"
+};
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Buket Ajda Tarik</Text>
       <StatusBar style="auto" />
     </View>
   );
